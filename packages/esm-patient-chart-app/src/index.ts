2c38554e65e64454638819df1a5a64d77b6cda8e
Thanusiya <thanusiya@senzmate.com>
2026-08-08 00:36:08 +0530

    Conditions based clinical view

--- packages/esm-patient-chart-app/src/index.ts
+++ packages/esm-patient-chart-app/src/index.ts
@@ -54,6 +54,11 @@ export const markPatientDeceasedActionButton = getSyncLifecycle(markPatientDecea
   moduleName,
 });
 
+export const clinicalViewsNavGroup = getAsyncLifecycle(
+  () => import('./clinical-views/clinical-views-nav-group.component'),
+  { featureName: 'clinical-views-nav-group', moduleName },
+);
+
 export const startVisitActionButton = getSyncLifecycle(startVisitActionButtonComponent, {
   featureName: 'patient-action-start-visit',
   moduleName,
